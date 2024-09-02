a4b60126bf9b395d9ae3cd27b42f96416a1b499d
Maximov Matvey <maximov-matvi@yandex.ru>
2024-09-02 17:45:00 +0400

    add doxygen to default.nix
    Add doxygen dependencies and add script for build documentation from
    latex.

--- default.nix
+++ default.nix
@@ -1,20 +1,30 @@
-{ lib, llvmPackages_14, cmake, boost, doxygen, ncurses }:
+{ lib, llvmPackages_14, cmake, boost, doxygen, gnumake, ncurses, texliveFull }:
 llvmPackages_14.stdenv.mkDerivation (finalAttrs: {
   pname = "find-faster-path";
   version = "0.0";
 
   src = ./.;
 
-  nativeBuildInputs = [ cmake doxygen ];
+  nativeBuildInputs = [ cmake doxygen gnumake texliveFull ];
   buildInputs = [ boost ncurses ];
 
   cmakeFlags = [ "-DBUILD_DOC=ON" ];
 
   installPhase = ''
-    echo $(pwd)
-    mkdir -p $out
-    cp ./main $out/find-faster-path
+    cd docs/doxygen/latex
+    make
+    cd ../../../
+
+    mkdir -p $out/bin $out/doc
+    cp ./main $out/bin/find-faster-path
+    cp docs/doxygen/latex/refman.pdf $out/doc/refman.pdf
   '';
+
+  env = {
+    TEXMFHOME = ".cache";
+    TEXMFVAR = ".cache/texmf-var";
+    # OSFONTDIR = "${pkgs.liberation_ttf}/share/fonts";
+  };
 
   meta = {
     description = "Find faster path on 2D map";
